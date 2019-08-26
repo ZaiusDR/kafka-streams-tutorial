d9b3d23d349a50103fa3df7ba739073f3260c289
Eduardo Suarez <eduardosuarez@gmail.com>
2019-08-26 20:49:03 +0200

    Add dependencies and log configuration

--- docker/docker_files/kafka_startup.sh
+++ docker/docker_files/kafka_startup.sh
@@ -7,7 +7,7 @@ KAFKA_CONFIG=${KAFKA_PATH}/config/server.properties
   echo ""
   echo "listeners=PLAINTEXT://:9092"
   echo advertised.listeners=PLAINTEXT://localhost:9092
-} >> ${KAFKA_CONFIG}
+} >>${KAFKA_CONFIG}
 
 # Start services
 ${KAFKA_PATH}/bin/zookeeper-server-start.sh ${KAFKA_PATH}/config/zookeeper.properties &
